43a4d4005b2a5e5a4553950e4ce6ca3aeddb2bf0
Naim Sen <naim.k.sen@gmail.com>
2018-11-03 18:02:15 +0000

    Added factory.py. Will contain class(es) for building typical box and dimension list objects.

---
+++ factory.py
@@ -0,0 +1,29 @@
+"""
+--Naim Sen--
+--Toby Ticehurst--
+Oct 18
+
+factory.py
+
+Class to generate boxes of a standard type.
+Also generates directions lists.
+"""
+
+from bounding_box import *
+import numpy as np
+
+
+class BoxFactory(object):
+    """
+    Factory for building specific boxes
+    """
+    def __init__(self, image):
+        # check image is valid
+        if np.any(image.shape) is 0:
+            raise ValueError("image dims must be non-zero")
+        self._image = image
+
+    # ~~ Properties -- #
+
+
+    

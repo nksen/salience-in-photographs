3457ad8633d4fbaf2e3eeb405722a9e59225db76
Naim Sen <naim.k.sen@gmail.com>
2018-11-28 17:39:03 +0000

    Added wrapping idea. Doesn't work as expected yet

--- text.py
+++ text.py
@@ -37,7 +37,35 @@ class Text(object):
         else:
             raise ValueError("font filetype must be .pil .ttf or . otf")
 
-        
+    def break_text(self, max_width):
+
+        # We share the subset to remember the last finest guess over 
+        # the text breakpoint and make it faster
+        print("aaaaaaaaaaaaa")
+        self._wrapped_text = self._raw_text
+        subset = len(self._wrapped_text)
+        letter_size = None
+
+        text_size = len(self._wrapped_text)
+        while text_size > 0:
+
+            # Let's find the appropriate subset size
+            while True:
+                width, height = self._font_obj.getsize(self._wrapped_text[:subset])
+                letter_size = width / subset
+
+                # min/max(..., subset +/- 1) are to avoid looping infinitely over a wrong value
+                if width < max_width - letter_size and text_size >= subset: # Too short
+                    subset = max(int(max_width * subset / width), subset + 1)
+                elif width > max_width: # Too large
+                    subset = min(int(max_width * subset / width), subset - 1)
+                else: # Subset fits, we exit
+                    break
+
+            yield self._wrapped_text[:subset]
+            self._wrapped_text = self._wrapped_text[subset:]   
+            text_size = len(self._wrapped_text)
+    
     # properties
     @property
     def raw_text(self):
@@ -49,14 +77,15 @@ class Text(object):
 
 
 if __name__ == "__main__":
-    raw = "This is a sentence."
+    raw = "This is a sentence that is going to be long This is a sentence that is going to be long This is a sentence that is going to be long This is a sentence that is going to be long."
     fontpath = PurePath("/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf")
     trialtext = Text(raw, fontpath, 40)
-    afont = trialtext.font_obj
 
+    trialtext.break_text(400)
+    #print(trialtext._wrapped_text)
     txt = ImageDraw.Image.new('RGBA', (500,500), (255,255,255,0))
     d = ImageDraw.Draw(txt)
-    d.text((10, 25), raw, font=afont)
-    imagenp = np.array(d)
-    print(imagenp)
+    for i, line in enumerate(trialtext.break_text(500)):
+        d.text((0, 16*i), line, (255,255,255), font=trialtext.font_obj)
+    #d.text((10, 25), raw, font=trialtext.font_obj)
     txt.show()

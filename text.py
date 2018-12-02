e9ddb940d8cbc4ee29c3dd9b54079cfe3bfe465d
Naim Sen <naim.k.sen@gmail.com>
2018-12-02 20:41:28 +0000

    Text now only handles paths to images and fonts rather than passing objects

--- text.py
+++ text.py
@@ -20,7 +20,7 @@ class Text(object):
     Text class. Handles all operations with text.
 
     """
-    def __init__(self, in_text, font_path, size_pt):
+    def __init__(self, in_text, font_path, size_pt, color=(0,0,0)):
         """
         Init function
         """
@@ -29,36 +29,25 @@ class Text(object):
         self._font_path = font_path
         self._font_size = size_pt
 
-        # load font
-        # get font type
+        # check font path has valid extension
         ext = font_path.suffix
-        if ext == ".pil":
-            self._font_obj = ImageFont.load(font_path)
-        elif ext == ".ttf" or ext == ".otf":
-            self._font_obj = ImageFont.truetype(str(font_path), size=size_pt)
-        else:
+        if not ext == ".pil" \
+        and not ext == ".otf" \
+        and not ext == ".ttf":
             raise ValueError("font filetype must be .pil .ttf or . otf")
     
-    # properties
+    def __str__(self):
+        return self._raw_text
+
+    # ~~ properties ~~ #
     @property
     def raw_text(self):
         return self._raw_text
     
-    @property
-    def font_obj(self):
-        return self._font_obj
-
-
+    # ~~ methods ~~ #
+    #def draw(self, img_path, )
 if __name__ == "__main__":
     raw = "This is a sentence that is going to be long This is a sentence that is going to be long This is a sentence that is going to be long This is a sentence that is going to be long."
     fontpath = PurePath("/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf")
     trialtext = Text(raw, fontpath, 40)
-
-    trialtext.break_text(400)
-    #print(trialtext._wrapped_text)
-    txt = ImageDraw.Image.new('RGBA', (500,500), (255,255,255,0))
-    d = ImageDraw.Draw(txt)
-    for i, line in enumerate(trialtext.break_text(500)):
-        d.text((0, 16*i), line, (255,255,255), font=trialtext.font_obj)
-    #d.text((10, 25), raw, font=trialtext.font_obj)
-    txt.show()
+    print(trialtext)

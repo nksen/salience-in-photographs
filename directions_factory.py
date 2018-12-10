bed2512ce7798191fc24fd790d3091b44dc81fd2
Naim Sen <naim.k.sen@gmail.com>
2018-12-10 17:28:02 +0000

    Added additionally constrained directions for corner-anchored boxes

--- directions_factory.py
+++ directions_factory.py
@@ -77,3 +77,44 @@ def bottom_anchored():
         ])
     return directions
 
+
+def topleft_anchored():
+    directions = np.array(
+        [
+            [[0, 0], [1, 0]],
+            [[0, 0], [0, 1]],
+            [[0, 0], [-1, 0]],
+            [[0, 0], [0, -1]]
+        ])
+        return directions
+
+
+def topright_anchored():
+    directions = np.array(
+        [
+            [[0, 0], [1, 0]],
+            [[0, -1], [0, 1]],
+            [[0, 0], [-1, 0]],
+            [[0, 1], [0, -1]]
+        ])
+    return directions
+
+
+def bottomleft_anchored():
+    directions = np.array(
+        [
+            [[-1, 0], [1, 0]],
+            [[0, 0], [0, 1]],
+            [[1, 0], [-1, 0]],
+            [[0, 0], [0, -1]]
+        ])
+
+
+def bottomright_anchored():
+    directions = np.array(
+        [
+            [[-1, 0], [1, 0]],
+            [[0, -1], [0, 1]],
+            [[1, 0], [-1, 0]],
+            [[0, 1], [0, -1]]
+        ])

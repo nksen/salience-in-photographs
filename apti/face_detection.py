ab055626ae888955d08bb9037b05d8f853576773
Naim Sen <naim.k.sen@outlook.com>
2019-03-05 19:01:01 +0000

    testing things

--- apti/face_detection.py
+++ apti/face_detection.py
@@ -1,18 +1,15 @@
 import cv2
 import numpy as np
-import face_recognition
+#import face_recognition
 
 
 if __name__ == "__main__":
 
-
-    file_name = "images/faces.jpg"
-
     # read in image (colour and grayscale needed)        
-    image = cv2.imread(file_name, 1)
+    image = cv2.imread(r"D:\Users\Naim\OneDrive\CloudDocs\UNIVERSITY\S8\MPhys_s8\test_images\alonso.jpg", 1)
 
     # openCV Method (1)
-    face_cascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_alt2.xml")
+    face_cascade = cv2.CascadeClassifier(r"C:\Users\Naim\Source\Repos\salience-in-photographs\apti\cascades\haarcascade_frontalface_alt2.xml")
 
     # format is (top left x, top left y, width, height)
     face_locations_1 = face_cascade.detectMultiScale(image, scaleFactor=1.5, minNeighbors=5)
@@ -28,7 +25,7 @@ if __name__ == "__main__":
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
-
+    """
     # Method 2
     # format is (top left y, top left x, bottom right y, bottom right x)
     face_locations_2 = face_recognition.face_locations(image)
@@ -44,7 +41,7 @@ if __name__ == "__main__":
         cv2.rectangle(image, (tl_x, tl_y), (br_x, br_y), (255, 0, 0), 3)
 
 
-
+    """
     cv2.imshow("Faces", image)
     cv2.waitKey(0)
     

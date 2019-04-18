e0ffbb040591ada0c80491167de04e8818436b7c
Naim Sen <naim.k.sen@outlook.com>
2019-04-18 23:49:28 +0100

    Testing out the analysis method

---
+++ aptipy/analysis/eye_tracking_analysis.py
@@ -0,0 +1,108 @@
+"""
+--Naim Sen--
+
+Apr 19
+
+Handles unpacking and analysing eye tracking data
+
+Copyright © 2019, Naim Sen
+Licensed under the terms of the GNU General Public License
+<https://www.gnu.org/licenses/gpl-3.0.en.html>
+"""
+
+#%%[markdown]
+# # Analysing eye tracking data
+# We want to compare our eye tracking results with the
+# boxes that we generated.
+#
+# Load in all the data...
+
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+
+plt.rcParams['figure.dpi'] = 170
+
+# Import data and images
+gaze_df = pd.read_excel(
+    r'D:\Users\Naim\OneDrive\CloudDocs\UNIVERSITY\S8\MPhys_s8\eyetracking_analysis\adjusted_gazedata.xlsx'
+)
+
+with open(
+        r'D:\Users\Naim\OneDrive\CloudDocs\UNIVERSITY\S8\MPhys_s8\eyetracking_analysis\images_dict.pkl',
+        'rb') as file:
+    imgs_dict = pickle.load(file)
+
+#%% [markdown]
+# ## Gaze heatmap
+# To generate a gaze heatmap we draw 2D Gaussians centred on each fixation, with x & y variances respectively.
+# The amplitude of each fixation is taken as the proportion that the fixation contributes to the total duration
+# of gaze gathered for the image as a whole. (Note: This is *not* per recording)
+#
+# Group data by image.
+
+#%%
+gaze_byimage = {img_name: df for img_name, df in gaze_df.groupby('MediaName')}
+
+#%%
+#from aptipy.analysis.utilities import draw_gauss2
+
+
+def draw_gauss2(gaze_map, datapoint, total_duration):
+    x0 = datapoint.FixationPointX
+    y0 = datapoint.FixationPointY
+    xvar = datapoint.FixationPointXErr**2
+    yvar = datapoint.FixationPointYErr**2
+    A = datapoint.GazeEventDuration / total_duration
+
+    img_xy = (datapoint.MediaWidth, datapoint.MediaHeight)
+
+    x = np.linspace(0, img_xy[0], 2000)
+    y = np.linspace(0, img_xy[1], 2000)
+
+    xx, yy = np.meshgrid(x, y)
+
+    datapoint_gauss = A * np.exp(-(((xx - x0)**2) / (2 * xvar) + (
+        (yy - y0)**2) / (2 * yvar)))
+
+    gaze_map += datapoint_gauss
+    # plt.figure()
+    # plt.contourf(x, y, datapoint_gauss)
+    # plt.colorbar()
+    # plt.xlim = img_xy[0]
+    # plt.ylim = img_xy[1]
+    # plt.errorbar(
+    #     x0,
+    #     y0,
+    #     fmt='none',
+    #     xerr=datapoint.FixationPointXErr,
+    #     yerr=datapoint.FixationPointYErr)
+
+
+for img_name, img_df in gaze_byimage.items():
+    # create canvas
+    gaze_map = np.zeros((2000, 2000))
+    # compute total duration recorded
+    duration = img_df['GazeEventDuration'].sum()
+    i_ = 0
+    for row in img_df.itertuples():
+        draw_gauss2(gaze_map, row, duration)
+
+        i_ += 1
+        if False:
+            plt.figure()
+            plt.imshow(gaze_map, extent=[0, 624, 0, 351], cmap='gray')
+            plt.colorbar()
+
+    # get image dims
+    img_shape = imgs_dict[img_name].shape
+
+    plt.figure()
+    plt.imshow(
+        gaze_map, extent=[0, img_shape[1], 0, img_shape[0]], cmap='Reds')
+    plt.colorbar()
+    plt.title(img_name)
+
+#%%
